a5c15c3a0821392ee07b3262f2d71d84cf3d6865
Brendan Ryan <brendan.ryan@pm.me>
2024-09-28 15:38:49 -0400

    Changed the font

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1 className='text-center text-danger'>Kandi Krazi</h1>
+        <small>Beautiful, unique, and handmade</small>
       </header>
     </div>
   )
